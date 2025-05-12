a14b01d7b5452c4cba2274669c413f7b48ab2423
wangjianlei <jianlei_wang7010@163.com>
2025-05-12 17:08:07 +0800

    first commit

---
+++ templates/default/src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { Viewer, BaseLayer } from 'larkexplorer-sdk-pro';
+import * as Cesium from 'cesium';
+import { nextTick, onMounted } from 'vue';
+onMounted(() => {
+  nextTick(() => {
+    const viewer: Viewer = new Viewer('globe', {
+      baseLayer: BaseLayer.DefaultSingleImg,
+      fpsShow: true,
+      mapboxController: true,
+    });
+    console.log(Cesium, viewer);
+  });
+});
+</script>
+
+<template>
+  <div id="globe"></div>
+</template>
+
+<style scoped>
+#globe {
+  width: 100%;
+  height: 100%;
+  position: relative;
+}
+</style>
